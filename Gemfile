9e696c6f48460409f113aec20ce1ee1f959fd29f
Luke <38197656+324Luke@users.noreply.github.com>
2019-05-28 11:24:04 -0400

    Add owner commands and HTTParty  gem

--- Gemfile
+++ Gemfile
@@ -3,4 +3,5 @@
 source 'https://rubygems.org'
 
 gem 'discordrb'
+gem 'httparty', '~> 0.17.0'
 gem 'redd'
---
+++ src/modules/commands/owner.rb
@@ -0,0 +1,45 @@
+module Bot::DiscordCommands
+  module Owner
+    extend Discordrb::Commands::CommandContainer
+    command(:o, help_available: false) do |event, command, *args|
+      break unless event.user.id == Bot::CONFIG.owner
+
+      case command
+      when 'eval'
+        begin
+          event.channel.send_embed do |embed|
+            embed.add_field(name: 'Input: ', value: "```\n#{args.join(' ')}```")
+            embed.add_field(name: 'Output: ', value: "```\n#{eval args.join(' ')}```")
+            embed.colour = 0x1f6d57
+          end
+        rescue => e
+          "An error occurred 😞 ```#{e}```"
+        end
+
+      when 'shutdown'
+        begin
+          event.channel.send_embed do |embed|
+            embed.title = 'Shutting down'
+            embed.description = 'The bot is shutting down...'
+            embed.color = 0x1f6d57
+          end
+
+          sleep 1
+          event.bot.invisible = true
+          bot.stop
+        rescue => e
+          "An error occurred 😞 ```#{e}```"
+        end
+
+      when 'reload'
+        begin
+          Bot.load_modules(:DiscordCommands, 'commands')
+          Bot.load_modules(:DiscordEvents, 'events', true)
+        rescue => e
+          "An error occurred 😞 ```#{e}```"
+        end
+
+      end
+    end
+  end
+end
