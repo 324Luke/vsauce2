b37722fafe203988e810091839e602996fffbead
Luke <38197656+324Luke@users.noreply.github.com>
2019-05-29 15:42:31 -0400

    Update help to include new animal commands

--- src/modules/commands/help.rb
+++ src/modules/commands/help.rb
@@ -7,12 +7,17 @@ module Bot::DiscordCommands
             usage: '') do |event|
       event.channel.send_embed do |embed|
         embed.title = '📒 VSauce Bot Help'
-        embed.description = 'Need more help than I can provide? Join our [support server](https://discord.gg/QWJGhbE)!'
+        embed.description = 'Need more help than I can provide? Join our [support server](https://discord.gg/ZRkSVjq)!'
 
         embed.add_field(name: "`#{Bot::CONFIG.prefix}help`", value: 'Shows a list of all the commands available for this server.', inline: false)
         embed.add_field(name: "`#{Bot::CONFIG.prefix}ping`", value: 'Gets the bots current ping to discord.', inline: false)
         embed.add_field(name: "`#{Bot::CONFIG.prefix}stats`", value: 'Returns basic statistics on the bot.', inline: false)
         embed.add_field(name: "`#{Bot::CONFIG.prefix}invite`", value: 'Returns an invite for the bot!', inline: false)
+        embed.add_field(name: "`#{Bot::CONFIG.prefix}dog`", value: '🐶 Returns a random dog image and fact!', inline: false)
+        embed.add_field(name: "`#{Bot::CONFIG.prefix}bird`", value: '🕊 Returns a random bird image and fact!', inline: false)
+        embed.add_field(name: "`#{Bot::CONFIG.prefix}cat`", value: '🐱 Returns a random cat image and fact!', inline: false)
+        embed.add_field(name: "`#{Bot::CONFIG.prefix}bunny`", value: '🐰 Returns a random bunny image and fact!', inline: false)
+        embed.add_field(name: "`#{Bot::CONFIG.prefix}duck`", value: '🦆 Returns a random duck image and fact!', inline: false)
 
         embed.color = Bot::CONFIG.color
         embed.timestamp = Time.now
