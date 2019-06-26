06f7a2f248279f074af848436121c3fc7e7cb99f
Luke <38197656+324Luke@users.noreply.github.com>
2019-06-26 02:08:40 -0400

    Add invite command

--- src/commands/invite.js
+++ src/commands/invite.js
@@ -1,4 +1,5 @@
 const { Command } = require('discord-akairo')
+const { embedColor } = require('../../data/config')
 
 class InviteCommand extends Command {
   constructor () {
@@ -8,7 +9,19 @@ class InviteCommand extends Command {
   }
 
   async exec (message) {
-    return message.channel.send('invite command ran')
+    const embed = this.client.util.embed()
+
+    embed.setTitle('➕ Invite')
+
+    embed.setDescription('Click [here](https://discordapp.com/api/oauth2/authorize?client_id=532710471367655425&permissions=104156224&scope=bot) to add the bot to your server!')
+
+    embed.setColor(embedColor)
+    embed.setTimestamp(new Date())
+    embed.setFooter(`Requested by ${message.author.username}#${message.author.discriminator}`, message.author.avatarURL)
+
+    return message.channel.send('', {
+      embed
+    })
   }
 }
 
