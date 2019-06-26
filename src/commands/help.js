15b9796698e9ba48d0d847f7b4156a7d9677bcf2
Luke <38197656+324Luke@users.noreply.github.com>
2019-06-26 02:49:40 -0400

    Add help command

--- src/commands/help.js
+++ src/commands/help.js
@@ -1,4 +1,5 @@
 const { Command } = require('discord-akairo')
+const { embedColor, commandPrefix } = require('../../data/config')
 
 class HelpCommand extends Command {
   constructor () {
@@ -10,11 +11,39 @@ class HelpCommand extends Command {
   async exec (message) {
     const embed = this.client.util.embed()
 
-    embed.setTitle('test')
+    embed.setTitle('📒 VSauce Bot Help')
+    embed.setDescription('Need more help than I can provide? Join our [support server](https://discord.gg/9fvBYnM)!')
 
-    return message.channel.send('', {
-      embed
-    })
+    /*
+     * Basic Commands
+     */
+    embed.addField(`\`${commandPrefix}help\``, 'Shows a list of all commands available to this server')
+    embed.addField(`\`${commandPrefix}ping\``, 'Gets the bots current ping to discord')
+    embed.addField(`\`${commandPrefix}stats, ${commandPrefix}info, ${commandPrefix}statistics\``, 'Returns basic statistics on the bot.')
+    embed.addField(`\`${commandPrefix}invite\``, 'Returns an invite for the bot')
+    /*
+     * Animal Commands
+     */
+    embed.addField(`\`${commandPrefix}dog, ${commandPrefix}doggo\``, '🐶 Returns a random dog image and fact!')
+    embed.addField(`\`${commandPrefix}bird, ${commandPrefix}birb\``, '🕊 Returns a random bird image and fact!')
+    embed.addField(`\`${commandPrefix}cat, ${commandPrefix}kitty\``, '🐱 Returns a random cat image and fact!')
+    embed.addField(`\`${commandPrefix}bunny\``, '🐰 Returns a random bunny image and fact!')
+    embed.addField(`\`${commandPrefix}duck\``, '🦆 Returns a random duck image and fact!')
+    /*
+     * Joke Commands
+     */
+    embed.addField(`\`${commandPrefix}insult\``, 'Insults anyone of your choosing!')
+    embed.addField(`\`${commandPrefix}gay\``, 'Returns a ~~100% accurate~~ value of how gay a given person is.')
+    embed.addField(`\`${commandPrefix}reverse, ${commandPrefix}nou\``, '🔃')
+    /*
+     * Reddit Commands
+     */
+
+    embed.setColor(embedColor)
+    embed.setTimestamp(new Date())
+    embed.setFooter(`Requested by ${message.author.username}#${message.author.discriminator}`, message.author.avatarURL)
+
+    return message.channel.send('', { embed })
   }
 }
 
