525cb5f25ed6e4f45a5629c48fbf976999b1e52e
Luke <38197656+324Luke@users.noreply.github.com>
2019-06-26 02:01:34 -0400

    Setup database initalization and start work on CRUD functions

---
+++ src/database.js
@@ -0,0 +1,65 @@
+/*
+ * Database controller
+ * Connects to a MongoDB instance and provides helper functions for maniuplating data
+ */
+
+const { database } = require('../data/config')
+const mongo = require('mongodb').MongoClient
+
+module.exports = {
+  async connect (url = database.url) {
+    return new Promise((resolve, reject) => {
+      mongo.connect(url, { useNewUrlParser: true })
+        .then(db => {
+          resolve(db)
+        })
+        .catch(err => {
+          reject(err)
+        })
+    })
+  },
+
+  /*
+   * CRUD (Create, Read, Update, Delete) functions
+   */
+
+  async create (collection, object) {
+    const db = await this.connect(database.url)
+    const data = db.db('vsauce').collection(collection)
+
+    return new Promise(async (resolve, reject) => {
+      await data.insertOne(object)
+    })
+  },
+
+  async read (collection, type = [ 'guild', 'user' ], id) {
+    const db = await this.connect(database.url)
+    const data = await db.db('vsauce').collection(collection)
+
+    return new Promise(async (resolve, reject) => {
+      await data.findOne({ id }, (err, item) => {
+        if (err) throw err
+
+        resolve(item)
+      })
+    })
+  },
+
+  async update (collection) {
+    const db = await this.connect(database.url)
+    const data = db.db('vsauce').collection(collection)
+
+    return new Promise(async (resolve, reject) => {
+
+    })
+  },
+
+  async delete (collection) {
+    const db = await this.connect(database.url)
+    const data = db.db('vsauce').collection(collection)
+
+    return new Promise(async (resolve, reject) => {
+
+    })
+  }
+}
