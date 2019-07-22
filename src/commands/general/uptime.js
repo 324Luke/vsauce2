b6bec6dc18852cbd566de98ae128c2a0b4ccc980
Luke <38197656+324Luke@users.noreply.github.com>
2019-07-22 14:44:26 -0400

    add uptime command

---
+++ src/commands/general/uptime.js
@@ -0,0 +1,34 @@
+import { Command } from 'discord-akairo'
+import { embedColor } from '@data/config'
+
+class UptimeCommand extends Command {
+  constructor () {
+    super('uptime', {
+      aliases: [ 'uptime' ],
+      category: 'misc'
+    })
+  }
+
+  async exec (message) {
+    let totalSeconds = (this.client.uptime / 1000)
+    let weeks = Math.floor(totalSeconds / 604800)
+    let days = Math.floor(totalSeconds / 86400)
+    let hours = Math.floor(totalSeconds / 3600)
+    totalSeconds %= 3600
+    let minutes = Math.floor(totalSeconds / 60)
+    let seconds = Math.floor(totalSeconds % 60)
+
+    const embed = this.client.util.embed()
+
+    embed.setTitle('⌚ Uptime')
+    embed.setDescription(`VSauce has been running for \`${(weeks === 0) ? '' : `${weeks}w:`}${(days === 0 ? '' : `${days}d:`)}${(hours === 0 ? '' : `${hours}h:`)}${(minutes === 0 ? '' : `${minutes}m:`)}${(seconds === 0 ? '' : `${seconds}s`)}\``)
+
+    embed.setColor(embedColor)
+    embed.setTimestamp(new Date())
+    embed.setFooter(`Requested by ${message.author.username}#${message.author.discriminator}`, message.author.avatarURL)
+
+    return message.channel.send({ embed })
+  }
+}
+
+export default UptimeCommand
