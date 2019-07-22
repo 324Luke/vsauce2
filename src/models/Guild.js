3de2b8184a74ac8f28e77941fd36832cc7873d67
Luke <38197656+324Luke@users.noreply.github.com>
2019-07-22 14:44:12 -0400

    add guild model

---
+++ src/models/Guild.js
@@ -0,0 +1,11 @@
+import Schema from 'mongoose'
+import Database from '../Database'
+
+const db = Database.connect()
+
+export default db.model('GuildModel', new Schema.Schema({
+  id: Number,
+  name: String,
+  prefix: Array(String),
+  disabledCommands: Array(String)
+}))
