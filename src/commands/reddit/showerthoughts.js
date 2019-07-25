89c19410e44840c1f6f06a72e552d41f1e0ac492
Luke <38197656+324Luke@users.noreply.github.com>
2019-07-25 14:57:29 -0400

    add shower thoughts command

---
+++ src/commands/reddit/showerthoughts.js
@@ -0,0 +1,31 @@
+import { Command } from 'discord-akairo'
+import { getReddit } from '@src/Utils'
+import { embedColor } from '@data/config'
+
+class ShowerThoughtsCommand extends Command {
+  constructor () {
+    super('showerthought', {
+      aliases: [ 'showerthought', 'showerthoughts' ],
+      category: 'reddit'
+    })
+  }
+
+  async exec (message) {
+    const post = await getReddit('showerthoughts')
+    const embed = this.client.util.embed()
+
+    if (!post.is_video && !post.over_18 && post.selftext.length < 2047 && !post.stickied) {
+      embed.setTitle(post.title)
+      embed.setURL(`https://reddit.com${post.permalink}`)
+      embed.setDescription(post.selftext)
+
+      embed.setColor(embedColor)
+      embed.setTimestamp(new Date())
+      embed.setFooter(`👍 ${post.score} | Requested by ${message.author.username}#${message.author.discriminator}`, message.author.avatarURL)
+
+      message.channel.send({ embed })
+    }
+  }
+}
+
+export default ShowerThoughtsCommand
